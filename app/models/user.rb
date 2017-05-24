ff1a81e58026877ad6f3c923dbf57bb855d7f797
mxdavis <md01@me.com>
2017-05-24 18:08:35 +0300

    add devise to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :carts
+
+  def current_cart=(current_cart)
+    self.carts
+  end
+
+  def current_cart
+    self.carts
+  end
 end
