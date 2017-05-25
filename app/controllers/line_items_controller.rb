8887b80d5d7f2119c07e447177a10793d1addb48
github username apalski <github email address annette.drapalski@gmail.com>
2017-05-25 10:56:00 +1000

    Added create method to line_items_controller, creates current_cart if it doesn't exist and adds items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,10 @@
 class LineItemsController < ApplicationController
 
 	def create
-		
+		new_current_cart unless current_cart
+		@line_item = current_cart.add_item(params[:item_id])
+		@line_item.save
+		current_cart.save
+		redirect_to cart_path(current_cart)
 	end
 end
