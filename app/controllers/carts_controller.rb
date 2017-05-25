53421c629611bf6cb39fe6fbd4e47f8f25a09483
github username apalski <github email address annette.drapalski@gmail.com>
2017-05-25 11:02:35 +1000

    Stubbed out carts controller new, create, checkout

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,24 @@
 class CartsController < ApplicationController
-  def show
-    current_cart
-  end
+  
+	def show
+		current_cart
+	end
+
+	def new
+		@cart = Cart.new
+	end
+
+	def create
+		@cart = Cart.new(cart_params)
+	end
+
+	def checkout
+		
+	end
+
+	private
+
+	def cart_params
+		params.require(:cart).permit(:user_id)
+	end
 end
